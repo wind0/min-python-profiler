6c4f4e0f98d8b9ae7df8830743b4da7828b9fbed
wind0 <romain.prou@gmail.com>
2021-01-21 10:14:20 +0100

    Testing environment update

--- __main__.py
+++ __main__.py
@@ -1,20 +1,32 @@
 import time
 import sys
-from timer import Profiler
+from timer import profile
 
 
+@profile
+def foo(n):
+    print('>>> foo')
+    bar(n / 4)
+    bar(n / 2)
+    baz()
+    time.sleep(n)
 
-def testfunc(num):
-    time.sleep(1)
-    print(f"{num}")
 
-def main():
-    profiler = Profiler()
-    sys.setprofile(profiler._profiler)
-    for i in range(10): 
-        testfunc(i)
-    sys.setprofile(None)
-    profiler.profiling_results_printing()
+def bar(n):
+    print('>>> bar')
+    time.sleep(n)
+    barsub()
+    barsub()
+
+
+def baz():
+    print('>>> baz')
+    time.sleep(0.050)
+
+def barsub():
+    print('>>>>>> barsub')
+    time.sleep(0.1)
+    
 
 if __name__ == "__main__":
-    main()
+    foo(1)
